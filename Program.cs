443874fa665369cf30a725a22f0ed5bad085a19d
srdjan <srdjan@timok.com>
2014-11-18 11:25:25 -0500

    standalone monads, removed inheritance

--- Program.cs
+++ Program.cs
@@ -49,78 +49,90 @@ namespace ConsoleApplication1 {
 		}
 
 		static void Main(string[] args) {
-			// Maybe monad usage: input regular string, intermediate result int...
+			//-1) Maybe monad usage: input regular string, intermediate result int...
 			var maybeM = new MaybeM<string>("I'm a string, wrapped up in the Maybe Monad!");
-			var result = maybeM.Bind(ExtractVowels)
-													 .Bind(Length);
-			Console.WriteLine("Result is: \{result.Value.Value}");
+			var result1 = maybeM.Bind(ExtractVowels)
+											 	  .Bind(Length);
+			Console.WriteLine("Result is: \{result1.Value}");
 
-			// Maybe monad usage: null string
+			//-2) Maybe monad usage: null string
 			maybeM = new MaybeM<string>(null);
 			var result2 = maybeM.Bind(ExtractVowels)
-											 .Bind(Length);
-			Console.WriteLine("Result is null?: \{result2.Value == null}");
+													.Bind(Length);
+			Console.WriteLine("Result is: \{result2.Value}");
 
-			// Writer monad usage
+			//-3) Writer monad usage
 			var writerM = new WriterM<string>("I'm a string, wrapped up in the Maybe Monad!");
 			var result3 = writerM.Bind(ExtractVowels)
-															 .Bind(Length);
-			Console.WriteLine("Result is: \{result3.Value.Value}");
-			foreach (var s in result3.Info) Console.WriteLine(s);
+													 .Bind(Length);
+			Console.WriteLine("Result is: \{result3.Value}");
 
 			Console.ReadLine();
 		}
 	}
 
+	//-- monad interface
+	//--
 	public interface IMonad<T> {
 		//note: Return is implemented by constructor
 		//IMonad<T> Return(T @value);
 		IMonad<T2> Bind<T2>(Func<T, T2> f) where T2 : class;
 
-		// public access to wrapped up value, optional but helpfull :)
-		T Value { get; }
-
-		// used in WriterM
-		List<string> Info { get; }
+		// simple public access the wrapped value(s), optional but helpfull :)
+		string Value { get; }
 	}
 
 	//-- Maybe monad
+	//--
 	class MaybeM<T> : IMonad<T> {
-		public T Value { get; }
-		public List<string> Info { get; internal set; }
+		internal T _value;
+		public string Value {	get { return _value == null ? "null" :  _value.ToString(); }}
 
-		public MaybeM(T @value) {
-			Value = @value;
-			Info = new List<string>();
+		public MaybeM(T value) {
+			_value = value;
 		}
 
-		public virtual IMonad<T2> Bind<T2>(Func<T, T2> f) where T2 : class {
-			if (Value != null) {
-				return new MaybeM<T2>(f(Value));
+		public IMonad<T2> Bind<T2>(Func<T, T2> f) where T2 : class {
+			if (_value != null) {
+				return new MaybeM<T2>(f(_value));
 			}
 			return new MaybeM<T2>(null);
 		}
 	}
 
-	//-- Writer + Maybe combined
-	class WriterM<T> : MaybeM<T> {
-		public WriterM(T @value) : base(@value) {	}
-
-		WriterM(T @value, List<string> info) : base(@value) {
-			Info = info;
+	//-- Writer monad 
+	//--
+	class WriterM<T> : IMonad<T> {
+		internal T _value;
+		public string Value {
+			get {
+				var valStr = _value.ToString();
+				_info.ForEach(s => valStr += ", " + s);
+				return valStr;
+			}
 		}
 
-		public override IMonad<T2> Bind<T2>(Func<T, T2> f) {
+		List<string> _info;
+
+		public WriterM(T @value) {
+			_value = value;
+			_info = new List<string>();
+		}
+
+		WriterM(T @value, List<string> info) {
+			_value = @value;
+			_info = info;
+		}
+
+		public IMonad<T2> Bind<T2>(Func<T, T2> f) where T2 : class {
 			try {
-				var result = base.Bind(f);
-				var w = new WriterM<T2>(result.Value, Info);
-				w.Info.Add("Result for \{(f.Method).Name}() is: \{result.Value.GetType()} -> \{result.Value}");
-				return w;
+				var result = (f(_value));
+				_info.Add("\{(f.Method).Name}()-> \{result}");
+				return new WriterM<T2>(f(_value), _info);
 			}
 			catch (Exception ex) {
-				var w = new WriterM<T2>(default(T2), Info);
-				w.Info.Add("Exception: \{ex.Message} thrown for \{(f.Method).Name}()");
-				return w;
+				_info.Add("Exception: \{ex.Message} thrown for \{(f.Method).Name}()");
+				return new WriterM<T2>(default(T2), _info);
 			}
 		}
 	}
